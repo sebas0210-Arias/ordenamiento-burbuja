37f112939b6af089db4450ce7240ff1af37af9b1
Sebastian Arias Ramirez <est.sebastian.ariasr@unimilitar.edu.co>
2026-04-09 10:15:04 -0500

    Se agrega el ordenamiento burbuja en java para arreglos de enteros

---
+++ OrdenamientoBurbuja/src/main/java/com/mycompany/ordenamientoburbuja/OrdenamientoBurbuja.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ordenamientoburbuja;
+import java.util.*;
+
+public class OrdenamientoBurbuja {
+
+    public static void main(String[] args) {
+       int [] arr= {45, 22, 11, -4, 7, 3};
+       
+       System.out.println(Arrays.toString(arr));
+       
+       int n = arr.length;
+       for(int i=0; i< n-1; i++){
+       for (int j =i+1; j < n; j++){
+           if(arr[i] > arr[j]) {
+               int aux = arr[j];
+               arr[j] = arr [i];
+               arr[i]= aux;
+           }
+       }
+       
+       
+       
+    }
+    }
+}
